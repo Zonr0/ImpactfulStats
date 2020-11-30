f2eebf4a3514a44f6c53797f7d4b007056821022
Andrew <zonrzero@gmail.com>
2020-11-29 18:31:18 -0800

    Some more experimentation

--- WishDistributions.py
+++ WishDistributions.py
@@ -8,18 +8,31 @@ from scipy import stats
 
 
 def roll_probability(n):
-    base_probability = 1 - math.pow(1 - 0.006, min(n,54))
+    """Returns the cumulative(?) probability of pulling a 5 star"""
+    p_normal = 0.006
+    p_pity = 0.33
+    non_five_star_chance = 1 - p_normal
+    non_five_star_chance_pity = 1 - p_pity
+    base_probability = math.pow(non_five_star_chance, min(n, 75))
     if n <= 75:
-        return base_probability
+        return 1 - base_probability
     # elif n == 90:
     #     return 1 -
     else:
         # Need to fix this. Plateus at 75. Probably some kind of double counting?
-        return base_probability * (1 - math.pow(1 - 0.33, n - 75))
+        return 1 - base_probability * (math.pow(non_five_star_chance_pity, n - 75))
 
+def individual_probability(n):
+    p = 0.006
+    q = 0.33
+    if n <= 75:
+        return -math.pow((1 - p),n) * math.log(1-p)
+    else:
+        return -math.pow((1 - p),75) * math.pow(1 - q,n-75) * math.log(1-q)
 
 def test_distributions():
     vroll = np.vectorize(roll_probability)
+    vind = np.vectorize(individual_probability)
 
     num_rolls = 90
     probability = 0.06
@@ -28,7 +41,7 @@ def test_distributions():
     ax1 = axs
 
     rolls = np.arange(1, num_rolls + 1)
-    ax1.bar(rolls, vroll(rolls))
+    ax1.bar(rolls, vind(rolls))
 
     plt.show()
 
